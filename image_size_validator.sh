e1c24b03546ea9f6bbd2b3fd28a38440881c276c
surajrawa <103116229+surajrawa@users.noreply.github.com>
2022-11-01 17:52:41 +0530

    Create image_size_validator.sh

---
+++ image_size_validator.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source variable
+
+for name in ${!IMAGES[@]}
+do
+    MAX_ALLOWED_IMAGE_SIZE=900000
+    IMAGE_SIZE=`docker  inspect -f "{{ .Size }}"  ${IMAGES[$name]}`
+    echo "CHECKING SIZE OF ${IMAGES[$name]}"
+    if [ $IMAGE_SIZE -gt $MAX_ALLOWED_IMAGE_SIZE ]
+        then 
+            # echo ${IMAGES[$name]} $IMAGE_SIZE >> file1.txt
+            echo "Warning: ${IMAGES[$name]} size  is greater than the expected limit"
+        else
+            echo "${IMAGES[$name]} size is in limits"
+    fi
+done
